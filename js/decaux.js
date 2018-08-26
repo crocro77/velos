8197196f6843cbda0ded7240eb640c1caec753d8
ntonyyy <anthony.fumo@gmail.com>
2018-08-26 22:38:06 +0200

    placement des marqueurs

--- js/decaux.js
+++ js/decaux.js
@@ -1,26 +1,48 @@
+	
+
 	$( document ).ready(function() {
 
-		getStationData();
+ 		
+	    init();
+
+
+		function init() {
+			let map;
+			loadMap();
+			getStationData();
+		}
+
+		function loadMap() {
+			mapboxgl.accessToken = 'pk.eyJ1IjoibnRvbnl5eSIsImEiOiJjamw2enA5eW8waGh0M3BvNXg0NXZieTliIn0.-3QrQ4Nba7o2SOaLyH7mIg';
+		    map = new mapboxgl.Map({
+			    container: 'map',
+			    style: 'mapbox://styles/mapbox/streets-v9',
+			    center: [4.83927, 45.750945],
+			    zoom: 11.5
+	    	});
+		}
+
+
+
 
 		function getStationData() {
 			var apiUrl = 'https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=525032ff79d1c138597fd9ea7f6640d8939eb118';
 
 			$.get( apiUrl, function( data ) {
-				for(var i = 0; i < data.length; i++ ) {
-				  var stationAddress = data[i].address;
-
-				  if (data[i].address !== "") {
-				   console.log( stationAddress + " index:"  + i);
-				  }
-				  else {
-				  	console.log(" Address empty !");
-				  }
-
+				for(i = 0; i < data.length; i++) {
+					setMarker(data[i].position.lat, data[i].position.lng);
 				}
 			});
-
-
-	
 		}
+
+
+		function setMarker(latitude, longitude) {
+
+			var marker = new mapboxgl.Marker()
+			  .setLngLat([longitude, latitude])
+			  .addTo(map);
+		}
+
+
 	  
 	});
