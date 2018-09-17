91a7ae3299dbced190eeeaf9bae06af5ee145c97
ntonyyy <anthony.fumo@gmail.com>
2018-09-17 15:09:32 +0200

    canvas refactoré objet

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,69 +1,76 @@
+function signatureCanvas(element) {
+    var $element = $(element);
+    var paint = false;
+    var context = $element[0].getContext("2d");
+    var line = [];
+    var clickDrag = [];
+    var clickX = [];
+    var clickY = [];
+
+    $element.mousedown(function (e) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+        paint = true;
+        addClick(mouseX, mouseY);
+        draw();
+    });
+
+    $element.mousemove(function (e) {
+        if (paint) {
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            draw();
+        }
+    });
+
+    $element.mouseup(function (e) {
+        paint = false;
+    });
+
+    $element.mouseleave(function (e) {
+        paint = false;
+    });
+
+    function addClick(x, y, dragging) {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+    }
+
+    function draw() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+        context.strokeStyle = "#000";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for (var i = 0; i < clickX.length; i++) {
+            context.beginPath();
+            if (clickDrag[i] && i) {
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+            } else {
+                context.moveTo(clickX[i] - 1, clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    }
+
+    this.clear = function () {
+        line.length = 0;
+        draw();
+    }
+
+    $("#clearCanvasBtn").click(function() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+        clickX = [];
+        clickY = [];
+        clickDrag = [];
+      });
+}
+
 $(document).ready(function () {
-
-  var context = document.getElementById('canvasWindow').getContext("2d");
-
-  $('#canvasWindow').mousedown(function (e) {
-    var mouseX = e.pageX - this.offsetLeft;
-    var mouseY = e.pageY - this.offsetTop;
-
-    paint = true;
-    addClick(mouseX, mouseY);
-    redraw();
-  });
-
-  $('#canvasWindow').mousemove(function (e) {
-    if (paint) {
-      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-      redraw();
-    }
-  });
-
-  $('#canvasWindow').mouseup(function (e) {
-    paint = false;
-  });
-
-  $('#canvasWindow').mouseleave(function (e) {
-    paint = false;
-  });
-
-  var clickX = new Array();
-  var clickY = new Array();
-  var clickDrag = new Array();
-  var paint;
-
-  function addClick(x, y, dragging) {
-    clickX.push(x);
-    clickY.push(y);
-    clickDrag.push(dragging);
-  }
-
-  function redraw() {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-
-    context.strokeStyle = "#000";
-    context.lineJoin = "round";
-    context.lineWidth = 5;
-
-    for (var i = 0; i < clickX.length; i++) {
-      context.beginPath();
-      if (clickDrag[i] && i) {
-        context.moveTo(clickX[i - 1], clickY[i - 1]);
-      } else {
-        context.moveTo(clickX[i] - 1, clickY[i]);
-      }
-      context.lineTo(clickX[i], clickY[i]);
-      context.closePath();
-      context.stroke();
-    }
-  }
-
-  $("#clearCanvasBtn").click(function() {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-    clickX = new Array();
-    clickY = new Array();
-    clickDrag = new Array();
-  });
-
-
-
+    var sign = new signatureCanvas('#canvasWindow');
+    $('#clearCanvasBtn').click(function () {
+        sign.clear();
+    })
 });
