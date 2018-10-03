7cd69f9523feffec79f65c7c9d7836a3ba9522f7
ntonyyy <anthony.fumo@gmail.com>
2018-10-03 23:25:25 +0200

    test loop slider

--- js/slider.js
+++ js/slider.js
@@ -21,16 +21,12 @@ function init() {
       }
     },
 
-    // loop: () => {
-    //   if (slider.index + 1 === slider.images.length) {
-    //     slider.index = 0;
-    //     document.getElementById('sliderImage').setAttribute('src', slider.images[slider.index]);
-    //   }
-    // },
-
     setSliderInterval: function () {
       return setInterval(function () {
         slider.next();
+        if (slider.index >= slider.images.length) {
+          slider.index = 0;
+        }
       }, 5000);
     }
   };
